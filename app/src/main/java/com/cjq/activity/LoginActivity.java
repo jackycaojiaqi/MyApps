fe10fbfb3e6c3deff2a93d71ce7f1cb98d99a04a
jackycaojiaqi <353938360@qq.com>
2015-11-13 14:39:12 +0800

    2015年11月13日14:38:57 欢迎界面

--- app/src/main/java/com/cjq/activity/LoginActivity.java
+++ app/src/main/java/com/cjq/activity/LoginActivity.java
@@ -1,23 +1,38 @@
 package com.cjq.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ImageView;
 
 import com.example.administrator.myapp.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+    private ImageView iv_logo;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+        context = this;
+        initview();
+    }
 
+    private void initview() {
+        iv_logo = (ImageView) findViewById(R.id.iv_logo);
+        iv_logo.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_logo:
+                //第三方登录
+                break;
+        }
     }
 }
--- app/src/main/java/com/cjq/activity/WelcomeActivity.java
+++ app/src/main/java/com/cjq/activity/WelcomeActivity.java
@@ -1,19 +1,51 @@
 package com.cjq.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Window;
 
+import com.cjq.domain.Constant;
 import com.example.administrator.myapp.R;
+
 
 /**
  * Created by Administrator on 2015/11/9.
  */
 public class WelcomeActivity extends Activity {
+    private Context context;
+    private  String TAG = "WelcomeActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_login);
+        context = this;
+        setContentView(R.layout.activity_welcome);
+        t.start();
     }
+
+    Handler mhandle = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Constant.MSG_SEND_INFO:
+                    Log.e(TAG,"=========handle");
+                    startActivity(new Intent(context, LoginActivity.class));
+                    finish();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+    Thread t = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            Log.e(TAG, "=========RUN");
+            mhandle.sendEmptyMessageDelayed(Constant.MSG_SEND_INFO, 3000);
+        }
+    });
 }
